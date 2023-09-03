2dd2443ddc0f9f67a5efeba9df4f52531e2118b5
TaarMaan <vlad-st000@yandex.ru>
2023-09-03 21:21:47 +0400

    add tests, add set/map/arrayList collections + test

--- src/test/java/ru/aston/artamonov_va/task1/ListUsersRailWaySuburbTicketTest.java
+++ src/test/java/ru/aston/artamonov_va/task1/ListUsersRailWaySuburbTicketTest.java
@@ -3,18 +3,23 @@ package ru.aston.artamonov_va.task1;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import ru.aston.artamonov_va.task1.dto.User;
+import ru.aston.artamonov_va.task1.exceptions.TicketException;
 import ru.aston.artamonov_va.task1.list.ListUsersTicket;
 import ru.aston.artamonov_va.task1.tickets.InternationalTicket;
 import ru.aston.artamonov_va.task1.tickets.SuburbTicket;
 
 import java.math.BigDecimal;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
 
-public class ListUsersRailWaySuburbTicketTest {
-    private ListUsersTicket listUsersTicket;
+class ListUsersRailWaySuburbTicketTest {
+    ListUsersTicket listUsersTicket;
+
+    List<RailWayTicket> testList;
+    List<RailWayTicket> testCollectionsList;
+    TreeSet<Integer> testSet;
+    Map<Integer, User> testMap;
 
     @BeforeEach
     void initListUsersTransportation() {
@@ -24,13 +29,33 @@ public class ListUsersRailWaySuburbTicketTest {
         User user4 = new User(35, "Грушева", "Галина");
         User user5 = new User(40, "Малина", "Мария");
 
-        List<RailWayTicket> list = new ArrayList<>();
-        list.add(new SuburbTicket(BigDecimal.valueOf(20), user1, 1));
-        list.add(new InternationalTicket(BigDecimal.valueOf(10), user2, 2));
-        list.add(new SuburbTicket(BigDecimal.valueOf(5), user3, 3));
-        list.add(new SuburbTicket(BigDecimal.valueOf(5), user4, 4));
-        list.add(new InternationalTicket(BigDecimal.valueOf(5), user5, 5));
-        listUsersTicket = new ListUsersTicket(list);
+        User testUser1 = new User(45, "Банана", "Бао");
+        User testUser2 = new User(50, "Арбузов", "Артем");
+
+        testList = new ArrayList<>();
+        testCollectionsList = new ArrayList<>();
+        testList.add(new SuburbTicket(BigDecimal.valueOf(20), user1, 1));
+        testList.add(new InternationalTicket(BigDecimal.valueOf(10), user2, 2));
+        testList.add(new SuburbTicket(BigDecimal.valueOf(5), user3, 3));
+        testList.add(new SuburbTicket(BigDecimal.valueOf(5), user4, 4));
+        testList.add(new InternationalTicket(BigDecimal.valueOf(5), user5, 5));
+
+        testCollectionsList.add(new SuburbTicket(BigDecimal.valueOf(20), testUser1, 1));
+        testCollectionsList.add(new InternationalTicket(BigDecimal.valueOf(10), testUser2, 2));
+
+        listUsersTicket = new ListUsersTicket(testList);
+
+        testSet = new TreeSet<>();
+        testSet.add(20);
+        testSet.add(10);
+        testSet.add(5);
+
+        testMap = new HashMap<>();
+        testMap.put(1, user1);
+        testMap.put(2, user2);
+        testMap.put(3, user3);
+        testMap.put(4, user4);
+        testMap.put(5, user5);
     }
 
     @Test
@@ -41,5 +66,30 @@ public class ListUsersRailWaySuburbTicketTest {
     @Test
     void sortList() {
         assertEquals(listUsersTicket.sortList().get(1).getUser().getSourName(), "Грушева");
+    }
+
+    @Test
+    void testTicketException() {
+        assertThrows(TicketException.class, () -> {
+            User user = new User(40, "Малина", "Мария");
+            RailWayTicket rwt = new InternationalTicket(BigDecimal.valueOf(0), user, 5);
+        });
+    }
+
+    @Test
+    void testArrayListEquals() {
+        assertNotEquals(testList, testCollectionsList);
+        testList.clear();
+    }
+
+    @Test
+    void testTreeSetPollLastMethod() {
+        assertEquals(testSet.pollLast(), 20);
+    }
+
+    @Test
+    void testHashMapContainsMethod() {
+        testMap.remove(3);
+        assertNull(testMap.get(3));
     }
 }
