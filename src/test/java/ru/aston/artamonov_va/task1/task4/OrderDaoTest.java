01808702204fc8d396f2f577551556db628b50c3
TaarMaan <vlad-st000@yandex.ru>
2023-09-13 16:55:15 +0400

    add task 5 (MockTests)

--- src/test/java/ru/aston/artamonov_va/task1/task4/OrderDaoTest.java
+++ src/test/java/ru/aston/artamonov_va/task1/task4/OrderDaoTest.java
@@ -1,52 +1,75 @@
 package ru.aston.artamonov_va.task1.task4;
 
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
 import ru.aston.artamonov_va.task1.task4.dao.OrderDao;
 import ru.aston.artamonov_va.task1.task4.domain.Order;
+import ru.aston.artamonov_va.task1.task4.repository.OrderRepository;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+@ExtendWith(MockitoExtension.class)
 class OrderDaoTest {
 
-    static OrderDao orderDao;
+    @Mock
+    OrderDao orderDao;
 
-    @BeforeAll
-    static void init() {
-        orderDao = new OrderDao();
-    }
+    @InjectMocks
+    OrderRepository orderRepository;
 
     @Test
     void findAll() {
-        List<Order> list = orderDao.findAll();
-        assertEquals(list.get(2).getName(), "Buy smurf acc");
+        List<Order> list = new ArrayList<>();
+
+        Order order1 = new Order(3, "Buy acc 6k pts");
+        Order order2 = new Order(4, "Buy acc 4k pts");
+        list.add(order1);
+        list.add(order2);
+
+        Mockito.when(orderDao.findAll()).thenReturn(list);
+        assertEquals(orderRepository.findAll().get(3).getName(), "Buy acc 6k pts");
     }
 
     @Test
     void findEntityById() {
         Order order = new Order(3, "Buy smurf acc 6.5k");
-        assertEquals(orderDao.findEntityById(order.getId()).getName(), "Buy smurf acc 6.5k");
+
+        Mockito.when(orderDao.findEntityById(order.getId())).thenReturn(order);
+        Order actual = orderRepository.findEntityById(order.getId());
+        assertEquals(actual.getName(), "Buy smurf acc 6.5k");
     }
 
     @Test
     void delete() {
-        assertTrue(orderDao.delete(2));
+        Mockito.when(orderDao.delete(2)).thenReturn(true);
+        assertTrue(orderRepository.delete(2));
     }
 
     @Test
     void update() {
-        Order order = new Order(3, "Buy smurf acc 6.5k");
-        assertEquals(orderDao.update(order).getName(), "Buy smurf acc 6.5k");
+        Order order = new Order(3, "Buy acc 6k pts");
+        Order orderNew = new Order(3, "Buy smurf acc 6.5k");
+
+        Mockito.when(orderDao.update(order)).thenReturn(orderNew);
+        assertEquals(orderRepository.update(order).getName(), orderNew.getName());
     }
 
     @Test
     void create() {
         Order order = new Order(1, "Buy smurf acc");
         Order order2 = new Order(2, "Buy smurf acc for brother");
-        assertTrue(orderDao.create(order));
-        assertTrue(orderDao.create(order2));
+
+        Mockito.when(orderDao.create(order)).thenReturn(true);
+        Mockito.when(orderDao.create(order2)).thenReturn(true);
+        assertTrue(orderRepository.create(order));
+        assertTrue(orderRepository.create(order2));
     }
 
 }
